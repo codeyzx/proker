0f40cc4d9510e2d2691bc485428ce802fb4899d3
codeyzx <yahyatruth@gmail.com>
2024-11-08 13:34:06 +0700

    update config

--- lib/utils/contants/app_constants.dart
+++ lib/utils/contants/app_constants.dart
@@ -1,23 +1,9 @@
 class AppConstants {
   // App
-  static const String appTitle = 'Moslem Traveler Demo';
+  static const String appTitle = 'Proker Hub';
 
   // App Config
   static const String defaultFontFamilyLightMode = 'Manrope';
   static const String defaultFontFamilyDarkMode = 'Manrope';
   static const String defaultFontFamily = 'Manrope';
-
-  // Networking and APIs
-  static const String backendUrl =
-      'https://demo-moeslimtravel.progantara.com/api';
-  static const String assetsUrl =
-      'https://demo-moeslimtravel.progantara.com/storage';
-
-  // Third-party services
-  static const int zegoCloudAppId = 1312967369;
-  static const String zegoCloudAppSign =
-      'cc3d870df0a16fbb8c087a71296fbca2c06fab760c0d19072c8653b6530fa9a0';
-
-  static const String mapboxAccessToken =
-      'sk.eyJ1IjoiYXBwc21henEiLCJhIjoiY2x5bjVoOWczMDI2ZjJ2czU3aWJ1bzZsZCJ9.I-9wGfF4lI5oHY-Jq-ELOQ';
 }
